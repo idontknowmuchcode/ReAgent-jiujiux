5471d6b6c72ef407af735424605bc5129eb6388b
tiquimimmi <165978862+tiquimimmi@users.noreply.github.com>
2024-12-12 21:16:23 -0600

    Edit to the sideeffect
    Edit to the sideeffect

--- SideEffects/DisplayTextSideEffect.cs
+++ SideEffects/DisplayTextSideEffect.cs
@@ -6,8 +6,25 @@ namespace ReAgent.SideEffects;
 
 [DynamicLinqType]
 [Api]
-public record DisplayTextSideEffect(string Text, Vector2 Position, string Color) : ISideEffect
+public record DisplayTextSideEffect : ISideEffect
 {
+    private readonly string Text;
+    private readonly Vector2 Position;
+    private readonly string Color;
+
+    public DisplayTextSideEffect(string text, Vector2 position, string color)
+    {
+        Text = text;
+        Position = position;
+        Color = color;
+    }
+
+    [Api]
+    public static DisplayTextSideEffect Create(string text, float x, float y, string color)
+    {
+        return new DisplayTextSideEffect(text, new Vector2(x, y), color);
+    }
+
     public SideEffectApplicationResult Apply(RuleState state)
     {
         state.InternalState.TextToDisplay.Add((Text, Position, Color));
